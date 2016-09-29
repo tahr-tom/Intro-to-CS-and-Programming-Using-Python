ec42b64f2a95d69aefb3fc7a5212011ed6700184
tahr-tom <thomaschan0818@gmail.com>
2016-09-29 18:30:26 +0800

    declare functions in a better way

--- Course/apply_to_each.py
+++ Course/apply_to_each.py
@@ -1,17 +1,22 @@
+# Return Abs
 test_list = [1, -4, 8, -9]
-def apply_to_each(L, f):
-    for i in range(len(L)):
-        L[i] = f(L[i])
+
+
+def apply_to_each(l, f):
+    for i in range(len(l)):
+        l[i] = f(l[i])
 apply_to_each(test_list, abs)
 
-
+# Return +1
 test_list = [1, -4, 8, -9]
-def add_one(target):
-    return target + 1
+
+
+def add_one(x): return x+1
 apply_to_each(test_list, add_one)
 
-
+# Return Square
 test_list = [1, -4, 8, -9]
-def square(target):
-    return target ** 2
+
+
+def square(target): return target ** 2
 apply_to_each(test_list, square)
