ca15750791ccfc3dae6c164906ed6b24f4d221cb
tahr-tom <thomaschan0818@gmail.com>
2016-10-03 01:54:30 +0800

    Code refactor for readability

--- Course/Polysum.py
+++ Course/Polysum.py
@@ -4,8 +4,10 @@ import math
 def polysum(n, s):
     """
     :param n: number of sides of a regular polygon, int
-    :param s: length of each slide
-    :return: area of that polygon
+    :param s: length of each slide, int
+    :return: sum of the area and square of the regular polygon's perimeter
     """
-    return round(((0.25 * n * s ** 2) / (math.tan(math.pi/n))) + (s * n) ** 2, 4)
-print(polysum(78, 40))
+    area = 0.25 * n * s ** 2 / math.tan(math.pi / n)
+    perimeter = (s * n) ** 2
+    ans = round(area + perimeter, 4)
+    return ans
